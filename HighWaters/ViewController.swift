7a34fddaa9891f83ec793afd1511701b37b37d9b
sochubert <sochubert@gmail.com>
2021-11-04 23:40:11 +0900

    init

---
+++ HighWaters/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  HighWaters
+//
+//  Created by Minjae Lee on 2021/11/04.
+//
+
+import UIKit
+import MapKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var mapView: MKMapView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
