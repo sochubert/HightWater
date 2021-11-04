6e9da84c5a429358a92b776908702f401195a6bb
sochubert <sochubert@gmail.com>
2021-11-05 00:20:52 +0900

    current location

--- HighWaters/ViewController.swift
+++ HighWaters/ViewController.swift
@@ -7,16 +7,33 @@
 
 import UIKit
 import MapKit
+import CoreLocation
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, CLLocationManagerDelegate, MKMapViewDelegate {
 
     @IBOutlet weak var mapView: MKMapView!
     
+    private var locationManager: CLLocationManager!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        self.locationManager = CLLocationManager()
+        self.locationManager.delegate = self
+        
+        // 위치 정확도 정도
+        self.locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        self.locationManager.distanceFilter = kCLDistanceFilterNone
+        
+        self.locationManager.requestWhenInUseAuthorization()
+        
+        self.mapView.showsUserLocation = true
+        self.mapView.delegate = self
+        
+        self.locationManager.startUpdatingLocation()
+        
     }
-
+    
 
 }
 
